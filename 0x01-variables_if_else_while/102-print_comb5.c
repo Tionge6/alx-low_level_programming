e0eba1ab323280a0557eb0e101ce35430952401f
Tionge6 <tionge362@gmail.com>
2022-07-01 17:17:30 -0700

    commit for today

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1/10) + '0');
 putchar((num1%10) + '0');
-putrchar(' ');
+putchar(' ');
 putchar((num2 % 10) + '0');
 putchar((num2 % 10) + '0');
 
