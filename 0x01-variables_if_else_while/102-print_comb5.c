9599ccc52c78b4dbd5b8d4585105fd5a4c46c726
Tionge6 <tionge362@gmail.com>
2022-07-01 17:40:07 -0700

    commit for today

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,9 +7,6 @@
 int main(void)
 {
 int num1, num2;
-const  char* stderr;
-stderr = '\0';
-fprintf( stderr, "[Anything]");
 for(num1 = 0; num1 <=98; num1++)
 {
 for (num2 = num1 + 1; num2 <= 99; num2++)
