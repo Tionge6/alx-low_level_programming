39e28d8c3af234dbecc17095eb45307ec2fd778a
Tionge6 <tionge362@gmail.com>
2022-07-12 13:27:56 -0700

    commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,17 +2,11 @@
 
 
 /**
-
  * swap_int - swaps the value of int a and int b
-
  * @a: first int to swap
-
  * @b: second int to swap
-
  *
-
  * Return: void
-
  */
 
 void swap_int(int *a, int *b)
